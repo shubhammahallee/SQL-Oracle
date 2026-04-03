c70469958d00ee03d12207ccfbb9ad836605172a
Shubham Mahalle <shubhammahallee@gmail.com>
2026-04-03 20:31:17 +0530

    Update basic_queries.sql

--- queries/basic_queries.sql
+++ queries/basic_queries.sql
@@ -16,3 +16,22 @@ select first_name,salary as orignal_salary ,(salary / 2) as increment_salary fro
 
 ---5. Calculate the annual salary of all employees (assuming monthly salary is stored in the table).
 select first_name,salary as orignal_salary ,(salary + 1000) as increment_salary from employees;
+
+---6. Write a query to calculate the difference between the highest and lowest salary in the table.
+SELECT first_name, salary
+FROM employees
+WHERE salary = (SELECT MAX(salary) FROM employees)
+   OR salary = (SELECT MIN(salary) FROM employees);
+
+---7. Display the square of the salary of all employees.   
+select first_name,salary as orignal_salary ,power(salary,2) as increment_salary from employees;
+
+---8. Add 2000 to the salary of employees in the IT department only.
+select first_name,department,salary as orignal_salary ,(salary + 2000) as increment_salary 
+from employees where department ='IT';
+
+---9. Write a query to calculate 10% of the salary of all employees and display it as "Tax".
+select first_name,salary as orignal_salary ,(salary * 10) as Tax from employees;
+
+---10. Find the result of (salary + 2000) * 1.1 for all employees and display it as "Adjusted Salary".
+select first_name,salary as orignal_salary ,(salary + 2000) * 1.1 as Adjusted_Salary from employees;
